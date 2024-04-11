34e5335a193451c4f1bc9584fe4a0ddceee56803
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-04-10 20:47:00 -0500

    Primeros cambios en Calculadora.vue

---
+++ calculadora-app/src/components/Calculadora.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="calculadora">
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+    <div>teste</div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+.calculadora {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
