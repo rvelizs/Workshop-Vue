3505721562d2363d699587611282e7bd5c708db7
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-04-10 22:06:19 -0500

    desarrollo de la unión de los números

--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculadora">
-    <div class="display">{{valorActual || '0'}}</div>
+    <div class="display">{{valorCorriente || '0'}}</div>
     <div v-on:click="limpiar" class="button">C</div>
     <div v-on:click="signo" class="button">+/-</div>
     <div v-on:click="porcentaje" class="button">%</div>
     <div v-on:click="n" class="button operator">÷</div>
-    <div v-on:click="n" class="button">7</div>
-    <div v-on:click="n" class="button">8</div>
-    <div v-on:click="n" class="button">9</div>
+    <div v-on:click="juntarNumeros('7')" class="button">7</div>
+    <div v-on:click="juntarNumeros('8')" class="button">8</div>
+    <div v-on:click="juntarNumeros('9')" class="button">9</div>
     <div v-on:click="n" class="button operator">x</div>
-    <div v-on:click="n" class="button">4</div>
-    <div v-on:click="n" class="button">5</div>
-    <div v-on:click="n" class="button">6</div>
+    <div v-on:click="juntarNumeros('4')" class="button">4</div>
+    <div v-on:click="juntarNumeros('5')" class="button">5</div>
+    <div v-on:click="juntarNumeros('6')" class="button">6</div>
     <div v-on:click="n" class="button operator">-</div>
-    <div v-on:click="n" class="button">1</div>
-    <div v-on:click="n" class="button">2</div>
-    <div v-on:click="n" class="button">3</div>
+    <div v-on:click="juntarNumeros('1')" class="button">1</div>
+    <div v-on:click="juntarNumeros('2')" class="button">2</div>
+    <div v-on:click="juntarNumeros('3')" class="button">3</div>
     <div v-on:click="n" class="button operator">+</div>
-    <div v-on:click="n" class="button zero">0</div>
+    <div v-on:click="juntarNumeros('0')" class="button zero">0</div>
     <div v-on:click="n" class="button">.</div>
-    <div v-on:click="n" class="button">=</div>
+    <div v-on:click="n" class="button operator"> = </div>
   </div>
 </template>
 
@@ -43,8 +43,16 @@ export default {
     porcentaje () {
       this.valorActual = `${parseFloat(this.valorActual) / 100}`
     },
+    juntarNumeros (numero) {
+      if (this.operatorClick) {
+        this.valorCorriente = numero;
+        this.operatorClick = false;
+      }
+
+      this.valorCorriente = `${this.valorCorriente}${numero}`;
+    },
     n () {
-      this.valorActual = true
+      this.valorCorriente = true
     }
   }
 }
@@ -60,9 +68,9 @@ export default {
   font-size: 40px;
 }
 .display {
-    grid-column: 1 / 5;
-    background-color: #333;
-    color: #fff;
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: #fff;
 }
 .zero {
   grid-column: 1 / 3;
