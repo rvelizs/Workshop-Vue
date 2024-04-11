158b9823c64c8c86d33715c90123a9e4632d091f
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-04-10 21:07:48 -0500

    Calculadora redimensionada, no funcional y bonita

--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculadora">
     <div class="display">0</div>
-    <div>C</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>÷</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>x</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
-    <div class="zero">0</div>
-    <div>.</div>
-    <div>=</div>
+    <div class="button">C</div>
+    <div class="button">+/-</div>
+    <div class="button">%</div>
+    <div class="button operator">÷</div>
+    <div class="button">7</div>
+    <div class="button">8</div>
+    <div class="button">9</div>
+    <div class="button operator">x</div>
+    <div class="button">4</div>
+    <div class="button">5</div>
+    <div class="button">6</div>
+    <div class="button operator">-</div>
+    <div class="button">1</div>
+    <div class="button">2</div>
+    <div class="button">3</div>
+    <div class="button operator">+</div>
+    <div class="button zero">0</div>
+    <div class="button">.</div>
+    <div class="button">=</div>
   </div>
 </template>
 
@@ -30,6 +30,8 @@ export default {
 
 <style scoped>
 .calculadora {
+  margin: 0 auto;
+  width: 350px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
@@ -42,5 +44,13 @@ export default {
 }
 .zero {
   grid-column: 1 / 3;
+}
+.button {
+  background-color: #f2f2f2;
+  border: 1px solid #333;
+}
+.operator {
+  background-color: #DF3C48;
+  color: white;
 }
 </style>
