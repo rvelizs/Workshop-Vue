34bb0fdfcc2fe65c46ff8556e739a89691c7b1d2
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-04-12 22:36:00 -0500

    resto de operaciones terminado

--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -8,15 +8,15 @@
     <div v-on:click="juntarNumeros('7')" class="button">7</div>
     <div v-on:click="juntarNumeros('8')" class="button">8</div>
     <div v-on:click="juntarNumeros('9')" class="button">9</div>
-    <div v-on:click="n" class="button operator">x</div>
+    <div v-on:click="multiplicar" class="button operator">x</div>
     <div v-on:click="juntarNumeros('4')" class="button">4</div>
     <div v-on:click="juntarNumeros('5')" class="button">5</div>
     <div v-on:click="juntarNumeros('6')" class="button">6</div>
-    <div v-on:click="n" class="button operator">-</div>
+    <div v-on:click="disminuir" class="button operator">-</div>
     <div v-on:click="juntarNumeros('1')" class="button">1</div>
     <div v-on:click="juntarNumeros('2')" class="button">2</div>
     <div v-on:click="juntarNumeros('3')" class="button">3</div>
-    <div v-on:click="n" class="button operator">+</div>
+    <div v-on:click="sumar" class="button operator">+</div>
     <div v-on:click="juntarNumeros('0')" class="button zero">0</div>
     <div v-on:click="punto" class="button">.</div>
     <div v-on:click="resultado" class="button operator"> = </div>
@@ -73,6 +73,18 @@ export default {
     dividir () {
       this.establecerValor()
       this.operator = (a, b) => a / b
+    },
+    multiplicar () {
+      this.establecerValor()
+      this.operator = (a, b) => a * b
+    },
+    disminuir () {
+      this.establecerValor()
+      this.operator = (a, b) => a - b
+    },
+    sumar () {
+      this.establecerValor()
+      this.operator = (a, b) => a + b
     },
     n () {
       this.valorActual = ''
